69cbd915c12e937104a3b334b5c2a258143efd91
Lancer <89342497+Scaresec-Lancer@users.noreply.github.com>
2022-07-01 14:32:34 +0800

    hhhhhhhhhhh

--- BT.go
+++ BT.go
@@ -2,27 +2,15 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"time"
+	"os"
+	"path/filepath"
 )
 
-func getTime() {
-	timeStr := time.Now().Format("2006-01-02 15:04")
-	fmt.Println(timeStr)
-}
-
-func getSys() (os, arch string, core int) {
-
-	//runtime.GOARCH 返回当前的系统架构；runtime.GOOS 返回当前的操作系统。
-	sysType := runtime.GOOS
-	sysArch := runtime.GOARCH
-	sysCore := runtime.NumCPU()
-
-	return sysType, sysArch, sysCore
-}
-
 func main() {
-	os, arch, core := getSys()
-	fmt.Println(os, arch, core)
-	getTime()
+
+	filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
+		path = filepath.ToSlash(path)
+		fmt.Println(path)
+		return nil
+	})
 }
